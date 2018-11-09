e63233a1769678b9e1f49209a499bd04f17de6e5
BryanKline36 <bryanjkline@gmail.com>
2018-11-08 20:10:47 -0800

    comments added, js changed

--- GameTools/MapEditor/mapEditor.js
+++ GameTools/MapEditor/mapEditor.js
@@ -62,8 +62,27 @@ function updateOne()
 // 	xmlhttp.open("GET", file, true);
 // 	xmlhttp.send();
 // }
+// function openfileDialog() {
+//     $("#fileLoader").click();
+// }
 
 
+var input = document.getElementById("myFile");
+var output = document.getElementById("output");
+
+
+input.addEventListener("change", function () {
+  if (this.files && this.files[0]) {
+    var myFile = this.files[0];
+    var reader = new FileReader();
+    
+    reader.addEventListener('load', function (e) {
+      output.textContent = e.target.result;
+    });
+    
+    reader.readAsBinaryString(myFile);
+  }   
+});
 
 
 var i;
