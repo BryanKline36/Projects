1fb47449fa8be5d39146bc188c1076605141427c
BryanKline36 <bryanjkline@gmail.com>
2018-11-23 14:18:07 -0800

    image file loader

--- GameTools/MapEditor/mapEditor.js
+++ GameTools/MapEditor/mapEditor.js
@@ -8,18 +8,21 @@ var stringValue = "";
 var input = document.getElementById("myFile");
 var output = document.getElementById("output");
 document.getElementById("load").addEventListener("click", drawImage);
+input.addEventListener("change", readInput);
 
 
 
 var imageList = [];
-var labelList = [];
+var labelList = ["w", "g"];
+loadImages();
 
 function loadImages()
 {
 	var labelSize = labelList.length;
-	var imageSize = imageList.length;
 	
-	for(i = imageSize; i < labelSize; i++)
+	imageList = [];
+	
+	for(i = 0; i < labelSize; i++)
 	{
 		imageList[i] = new Image();
 		imageList[i].src = labelList[i] + ".png";
@@ -28,15 +31,8 @@ function loadImages()
 
 }
 
-// var grassImage = new Image();
-// grassImage.src = "g.png";
-// var waterImage = new Image();
-// waterImage.src = "w.png";
 
-
-
-input.addEventListener("change", 
-function() 
+function readInput() 
 {
 	if (this.files && this.files[0]) 
 	{
@@ -44,29 +40,23 @@ function()
 		var reader = new FileReader();
 		
 		reader.addEventListener('load', 
-		function(e) 
-		{
-			output.textContent = e.target.result;
-		});
+			function(e) 
+			{
+				output.textContent = e.target.result;
+			});
 		
 		reader.readAsBinaryString(myFile);
 	}   
-});
+}
 
 
 
 function readMap()
 {
-	var mapText = document.getElementById("output").value
-	
-	map = mapText;
-	formatMapString();
-}
-
-function formatMapString()
-{
-	var i , j;
+	var i;
 	var tempString = "";
+
+	map = document.getElementById("output").value
 	
 	for(i = 0; i < 272; i++)
 	{
@@ -80,18 +70,22 @@ function formatMapString()
 }
 
 
+
+
 function getUniqueTiles()
 {
-	var size = labelList.length;
+	//var size = labelList.length;
 
-	console.log(size);
+	//console.log(size);
 
 	for(i = 0; i < 256; i++)
 	{
-		if(!labelExists(map[i]))
-		{
-			labelList.push(map[i]);
-		}
+
+
+		// if(!labelExists(map[i]))
+		// {
+		// 	labelList.push(map[i]);
+		// }
 	}
 
 	console.log(labelList)
@@ -116,7 +110,8 @@ function drawImage()
 	var xPosition = 0, yPosition = 0;
 	
 	readMap();
-	getUniqueTiles();
+	//getUniqueTiles();
+	loadImages();
 	
 	for(i = 0; i < 256; i++)
 	{
@@ -138,138 +133,5 @@ function drawImage()
 		xPosition += 32;
 	}
 }
-
-
-
-
-
-// document.getElementById('files').addEventListener('change', handleFileSelect, false);
-// function handleFileSelect(evt) 
-// {
-// 	console.log("!!!!!!")
-// 	var files = evt.target.files; // FileList object
-
-// 	// files is a FileList of File objects. List some properties.
-// 	var output = [];
-// 	var f;
-// 	for (var i = 0; f = files[i]; i++) 
-// 	{
-
-// 		console.log(f.name)
-// 		// output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
-// 		//     f.size, ' bytes, last modified: ',
-// 		//     f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
-// 		//     '</li>');
-// 	}
-// 	// document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// writeMapToScreen(map.join(""));
-
-
-
-// gameWindow.fillStyle = "#801000";
-// gameWindow.fillRect(0, 0, 1000, 400);
-
-// gameWindow.fillStyle = "#000000";
-// gameWindow.fillRect(0, 0, 40, 40);
-
-// var things = new Image(100, 100);
-// things.src = "images/w.png";
-// gameWindow.drawImage(things, 0, 0);
-
-
-// var imageObject = new Image();
-// imageObject.src = "images/w.png";
-// gameWindow.drawImage(imageObject, 0, 0);
-
-
-
-
-// function gameStatusInterval()	
-// { 
-//   	setInterval(update, 40); 
-// }
-
-// function update()
-// {
-// 	gameWindow.fillStyle = "#801000";
-// 	gameWindow.fillRect(0, 0, 1000, 400);
-// }
-
-// gameStatusInterval();
-
-// $(document).keyup(function(key) 
-// {
-// 	if(key.keyCode == 37)
-// 	{
-// 		settings.canvasOne.x_speed = 2;
-// 		LEFT = false;
-// 	}
-// 	else if(key.keyCode == 39)	
-// 	{	
-// 		settings.canvasOne.x_speed = 2;
-// 		RIGHT = false;
-// 	}
-// 	else if(key.keyCode == 38)
-// 	{
-// 		settings.canvasOne.y_speed = 2;
-// 		UP = false;
-// 	}
-// 	else if(key.keyCode == 40)
-// 	{
-// 		settings.canvasOne.y_speed = 2;
-// 		DOWN = false;
-// 	}
-// });
-
-// function changeSettings(input)
-// {
-// 	var xmlhttp = new XMLHttpRequest();
-
-// 	xmlhttp.onreadystatechange = function() 
-// 	{
-// 		if(this.readyState == 4 && this.status == 200) 
-// 		{
-// 			settings = JSON.parse(this.responseText);
-// 		}
-// 	};
-
-// 	if(input == 0)
-// 	{	
-// 		var file = "defaultSettings.JSON";
-// 	}
-// 	else if(input == 1)
-// 	{
-// 		var file = "altSettings.JSON";
-// 	}
-
-// 	xmlhttp.open("GET", file, true);
-// 	xmlhttp.send();
-// }
-// function openfileDialog() {
-//     $("#fileLoader").click();
-// }
-
-
-// var map = document.getElementById("mapCanvas");
-// var ctx = map.getContext("2d");
-
-
-// var baseImage = new Image();
-// baseImage.src = "g.png";
-// baseImage.addEventListener("load", drawImage);
 
 
