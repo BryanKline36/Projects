3ac0e35a582063d93145dce116bfe062d964b360
BryanKline36 <bryanjkline@gmail.com>
2018-11-23 17:08:58 -0800

    image file loader

--- GameTools/MapEditor/mapEditor.js
+++ GameTools/MapEditor/mapEditor.js
@@ -1,20 +1,17 @@
 var demoCanvas = document.getElementById("mapCanvas"); 
 var gameWindow = demoCanvas.getContext("2d");
 
-var i, j;
-var rows = [];
+var COLUMN_SIZE = 16;
+var STEP_SIZE = 32;
+
 var map = [];
-var stringValue = "";
+var imageList = [];
+var labelList = [];
 var input = document.getElementById("myFile");
 var output = document.getElementById("output");
+
 document.getElementById("load").addEventListener("click", drawImage);
 input.addEventListener("change", readInput);
-
-
-
-var imageList = [];
-var labelList = ["w", "g"];
-loadImages();
 
 function loadImages()
 {
@@ -27,38 +24,34 @@ function loadImages()
 		imageList[i] = new Image();
 		imageList[i].src = labelList[i] + ".png";
 	}
-
-
 }
-
 
 function readInput() 
 {
-	if (this.files && this.files[0]) 
+	if(this.files && this.files[0]) 
 	{
 		var myFile = this.files[0];
 		var reader = new FileReader();
 		
 		reader.addEventListener('load', 
-			function(e) 
+			function(parameter) 
 			{
-				output.textContent = e.target.result;
+				output.textContent = parameter.target.result;
 			});
 		
 		reader.readAsBinaryString(myFile);
 	}   
 }
 
-
-
 function readMap()
 {
-	var i;
+	var i, size;
 	var tempString = "";
 
 	map = document.getElementById("output").value
+	size = map.length;
 	
-	for(i = 0; i < 272; i++)
+	for(i = 0; i < size; i++)
 	{
 		if(map[i] != '\n')
 		{
@@ -69,69 +62,38 @@ function readMap()
 	map = tempString;
 }
 
-
-
-
-function getUniqueTiles()
-{
-	//var size = labelList.length;
-
-	//console.log(size);
-
-	for(i = 0; i < 256; i++)
-	{
-
-
-		// if(!labelExists(map[i]))
-		// {
-		// 	labelList.push(map[i]);
-		// }
-	}
-
-	console.log(labelList)
-}
-
-function labelExists(tileChar)
-{
-	for(i = 0; i < 256; i++)
-	{
-		if(map[i] == tileChar)
-		{
-			return true;
-		}
-	}
-
-	return false;
-}
-
 function drawImage()
 {
-	var i;
+	var i, size = map.length;
 	var xPosition = 0, yPosition = 0;
+	var tempImage = new Image();
 	
 	readMap();
-	//getUniqueTiles();
+
+	labelList = Array.from(new Set(map.split('')));
+
 	loadImages();
 	
-	for(i = 0; i < 256; i++)
+	for(i = 0; i < size; i++)
 	{
-		if(i != 0 && i % 16 == 0)
+		if(i != 0 && i % COLUMN_SIZE == 0)
 		{
 			xPosition = 0;
-			yPosition += 32;
+			yPosition += STEP_SIZE;
 		}
 		
-		if(map[i] == labelList[0])
-		{
-			gameWindow.drawImage(imageList[0], xPosition, yPosition);
-		}
-		else if(map[i] == labelList[1])
-		{
-			gameWindow.drawImage(imageList[1], xPosition, yPosition);
-		}
-		
-		xPosition += 32;
+		tempImage = getImage(i);
+		gameWindow.drawImage(tempImage, xPosition, yPosition);
+
+		xPosition += STEP_SIZE;
 	}
 }
 
+function getImage(index)
+{
+	var charTile = map[index];
+	
+	index = labelList.indexOf(charTile);
 
+	return imageList[index];
+}
