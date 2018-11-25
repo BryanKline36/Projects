15f54963e5c2302187cad54965b31a6df6d8054e
BryanKline36 <bryanjkline@gmail.com>
2018-11-25 14:58:54 -0800

    changing to pygame

--- GameTools/MapEditor/testGame.py
+++ GameTools/MapEditor/testGame.py
@@ -1,11 +1,53 @@
 import pygame
 
 pygame.init()
-window = pygame.display.set_mode((800, 600))
+window = pygame.display.set_mode((512, 512))
+
+waterimage = pygame.image.load("w.png")
+grassimage = pygame.image.load("g.png")
+
+map = []
+
+def drawImage():
+
+    xPosition = 0 
+    yPosition = 0
+
+    for i in range(256):
+
+        if i != 0 and i % 16 == 0:
+
+            xPosition = 0
+            yPosition += 32
+
+        window.blit(waterimage, (xPosition, yPosition))
+        pygame.display.flip()
+
+        xPosition += 32
+
+def readMapFile():
+
+    file = open("testMap.map", "r")
+
+    for item in file:
+        map.append(item[:-1])
+
+
+readMapFile()
+
 while True:
+
+
+
+
+
+    drawImage()
+
+
+    # window.fill(black)
+
     for event in pygame.event.get():
-        print(event)
         if event.type == pygame.QUIT:
-            print("dddddddddddddddd")
             
+            print(map)
             pygame.quit()
