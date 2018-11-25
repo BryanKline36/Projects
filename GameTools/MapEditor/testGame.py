50a53328847e0dee2ea918e4d7a1d470d1bc7ba9
BryanKline36 <bryanjkline@gmail.com>
2018-11-25 15:40:10 -0800

    changing to pygame

--- GameTools/MapEditor/testGame.py
+++ GameTools/MapEditor/testGame.py
@@ -1,17 +1,24 @@
 import pygame
 
+
+
 pygame.init()
-window = pygame.display.set_mode((512, 512))
+window = pygame.display.set_mode((1024, 512))
 
 waterimage = pygame.image.load("w.png")
 grassimage = pygame.image.load("g.png")
 
 map = []
 
+                 
+
+
+
 def drawImage():
 
     xPosition = 0 
     yPosition = 0
+    image = None
 
     for i in range(256):
 
@@ -20,7 +27,14 @@ def drawImage():
             xPosition = 0
             yPosition += 32
 
-        window.blit(waterimage, (xPosition, yPosition))
+        if map[i] == "w":
+
+            window.blit(waterimage, (xPosition, yPosition))
+
+        if map[i] == "g":
+
+            window.blit(grassimage, (xPosition, yPosition))
+
         pygame.display.flip()
 
         xPosition += 32
@@ -29,19 +43,37 @@ def readMapFile():
 
     file = open("testMap.map", "r")
 
-    for item in file:
-        map.append(item[:-1])
+    for line in file:
+
+        for char in line:
+
+            if char != '\n':    
+
+                map.append(char)
 
 
 readMapFile()
 
+
+
+
+def message_display(text):
+    largeText = pygame.font.Font('freesansbold.ttf', 16)
+    TextSurf, TextRect = text_objects(text, largeText)
+    TextRect.center = (576, 64)
+    window.blit(TextSurf, TextRect)
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, (255,255,255))
+    return textSurface, textSurface.get_rect()    
+
 while True:
 
-
-
-
-
     drawImage()
+    message_display("thing")
+
+
+    pygame.display.flip()
 
 
     # window.fill(black)
