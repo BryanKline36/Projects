3d9c9ab004e9e21c4b96eed1c029ddd7ca89da0b
BryanKline36 <bryanjkline@gmail.com>
2018-12-24 13:22:24 -0800

    game of life

--- Cellular Automata/ConwaysLife.py
+++ Cellular Automata/ConwaysLife.py
@@ -5,10 +5,16 @@ window = pygame.display.set_mode((512, 640))
 
 cells = []
 
-for index in range(0, 64):
-    cells[index] = False
+liveCell = pygame.image.load("alive.png").convert_alpha()
 
+for index in range(0, 4096):
+    cells.append(False)
+    
+    window.blit(liveCell, ((index % 64) * 8, (index / 64) * 8))
 
+pygame.draw.rect(window, (128, 128, 128), [0, 512, 512, 128])
+
+pygame.display.flip()
 
 while True:
 
