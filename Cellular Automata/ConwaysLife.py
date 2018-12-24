43380744fcae592302a0d27405551d5200841445
BryanKline36 <bryanjkline@gmail.com>
2018-12-24 13:45:11 -0800

    game of life

--- Cellular Automata/ConwaysLife.py
+++ Cellular Automata/ConwaysLife.py
@@ -3,6 +3,18 @@ import pygame
 pygame.init()
 window = pygame.display.set_mode((512, 576))
 
+
+def drawText(text, xPostion, yPosition):
+
+    textFont = pygame.font.Font('freesansbold.ttf', 16)
+    textSurface = textFont.render(text, True, (0, 0, 0))
+
+    textRectangle = textSurface.get_rect() 
+    textRectangle.left = xPostion
+    textRectangle.top = yPosition
+
+    window.blit(textSurface, textRectangle)
+
 cells = []
 
 liveCell = pygame.image.load("alive.png").convert_alpha()
@@ -16,10 +28,19 @@ for index in range(0, 4096):
 
 pygame.draw.rect(window, (128, 128, 128), [0, 512, 512, 128])
 
-window.blit(button, (32, 544))
-window.blit(button, (160, 544))
-window.blit(button, (288, 544))
-window.blit(button, (416, 544))
+window.blit(button, (32, 528))
+window.blit(button, (160, 528))
+window.blit(button, (288, 528))
+window.blit(button, (416, 528))
+
+drawText("Start", 45, 535)
+drawText("Stop", 175, 535)
+drawText("Faster", 297, 535)
+drawText("Slower", 422, 535)
+
+
+
+
 
 
 pygame.display.flip()
