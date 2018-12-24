350b22b14a25823a829b01f0e6a82f1adf51e1dc
BryanKline36 <bryanjkline@gmail.com>
2018-12-24 13:34:06 -0800

    game of life

--- Cellular Automata/ConwaysLife.py
+++ Cellular Automata/ConwaysLife.py
@@ -1,11 +1,13 @@
 import pygame
 
 pygame.init()
-window = pygame.display.set_mode((512, 640))
+window = pygame.display.set_mode((512, 576))
 
 cells = []
 
 liveCell = pygame.image.load("alive.png").convert_alpha()
+deadCell = pygame.image.load("dead.png").convert_alpha()
+button = pygame.image.load("button.png").convert_alpha()
 
 for index in range(0, 4096):
     cells.append(False)
@@ -13,6 +15,12 @@ for index in range(0, 4096):
     window.blit(liveCell, ((index % 64) * 8, (index / 64) * 8))
 
 pygame.draw.rect(window, (128, 128, 128), [0, 512, 512, 128])
+
+window.blit(button, (32, 544))
+window.blit(button, (160, 544))
+window.blit(button, (288, 544))
+window.blit(button, (416, 544))
+
 
 pygame.display.flip()
 
