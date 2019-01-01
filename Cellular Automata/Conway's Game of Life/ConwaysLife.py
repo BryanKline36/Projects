b0828ef9ea68fd45768cac089dc8564afad83c7c
BryanKline36 <bryanjkline@gmail.com>
2019-01-01 12:28:25 -0800

    update clock

--- Cellular Automata/Conway's Game of Life/ConwaysLife.py
+++ Cellular Automata/Conway's Game of Life/ConwaysLife.py
@@ -5,186 +5,6 @@ import Automaton
 from math import sqrt
 from time import sleep
 
-# # CellGrid class definition
-
-# class CellGrid:
-
-#     grid = None
-#     gridSize = None
-#     offset = None
-#     current = None
-#     dead = None
-#     alive = None
-
-#     def __init__(self, size):
-
-#         self.grid = []            
-#         self.gridSize = int(size)
-#         self.offset = int(sqrt(size))
-#         self.current = False
-#         self.dead = False
-#         self.alive = True
-
-#         for index in range(0, self.gridSize):
-#             self.grid.append(self.dead)
-
-#     def clearGrid(self):
-
-#         for index in range(0, self.gridSize):
-#             self.grid[index] = self.dead
-
-#     def setCurrent(self, current):
-
-#         self.current = current
-
-#     def toggleCell(self, index):
-
-#         if index >= 0 and index < self.gridSize:
-#             self.grid[index] = self.grid[index] ^ True
-
-#     def getNeighborIndices(self, index):
-
-#         neighborIndices = []
-#         neighbors = [(index - self.offset - 1), (index - self.offset), (index - self.offset + 1), 
-#                     (index - 1), index, (index + 1), 
-#                     (index + self.offset - 1), (index + self.offset), (index + self.offset + 1)]
-
-#         if index >= 0 and index < self.gridSize:
-#             for position in neighbors:
-#                 if position >= 0 and position < self.gridSize:
-#                     neighborIndices.append(position)
-
-#         return neighborIndices
-
-#     def getLiveNeighborCount(self, index):
-
-#         liveNeighbors = 0
-
-#         if index >= 0 and index < self.gridSize:
-#             neighborIndices = self.getNeighborIndices(index)
-
-#             for neighborIndex in neighborIndices:
-#                 if self.grid[neighborIndex]:
-#                     liveNeighbors += 1
-
-#         return liveNeighbors
-
-#     def applyRules(self, index):
-
-#         state = False
-
-#         if index >= 0 and index < self.gridSize:
-#             neighborCount = self.getLiveNeighborCount(index)
-            
-#             if self.grid[index] and (neighborCount < 2 or neighborCount > 3):
-#                 state = self.dead
-
-#             if (not self.grid[index]) and (neighborCount > 1 and neighborCount < 4):
-#                 state = self.alive
-
-#         return state
-
-#     def setCellState(self, index, state):
-
-#         self.grid[index] = state
-
-#     def printGrid(self):
-
-#         print("Grid Start:")
-        
-#         for index in range(0, self.gridSize):
-#             print(self.grid[index])
-        
-#         print("Grid End")    
-
-# # Automaton class definition
-
-# class Automaton:
-
-#     window = None
-#     grids = None
-#     currentGridIndex = None
-#     gridSize = None
-#     cellSize = None
-#     aliveImage = None
-#     deadImage = None
-
-#     def __init__(self, window, size):
-
-#         self.window = window
-#         self.grids = []
-#         self.grids.append(CellGrid(size))
-#         self.grids.append(CellGrid(size))
-#         self.grids[0].setCurrent(True)
-#         self.grids[1].setCurrent(False)
-#         self.currentGridIndex = 0
-#         self.gridSize = size
-#         self.gridSide = int(sqrt(self.gridSize))
-#         self.cellSize = int(sqrt(self.gridSide))
-
-#     def setImages(self, paths):
-
-#         if len(paths) == 2:
-#             self.aliveImage = paths[0]
-#             self.deadImage = paths[1]
-
-
-#     def getCurrentGrid(self):
-
-#         if self.grids[0].current and not self.grids[1].current:
-#             self.currentGridIndex = 0 
-#         elif not self.grids[0].current and self.grids[1].current:
-#             self.currentGridIndex = 1
-
-#     def setCurrentGrid(self, index):
-#         if index >= 0 and index < 2:
-#             self.currentGridIndex = index
-#             self.grids[index].setCurrent(True)    
-#             self.grids[index ^ 1].setCurrent(False)    
-
-#     def updateGrid(self):
-        
-#         self.getCurrentGrid()
-#         for index in range(0, self.gridSize):
-#             print(self.currentGridIndex ^ 1)
-#             self.grids[self.currentGridIndex ^ 1].setCellState(index, self.grids[self.currentGridIndex].applyRules(index)) 
-
-#         self.grids[self.currentGridIndex].clearGrid()  
-#         self.setCurrentGrid(self.currentGridIndex ^ 1)
-
-#     def drawGrid(self):
-
-#         if self.window != None:
-#             for index in range(0, self.gridSize):
-#                 xPosition = (index % self.gridSide) * self.cellSize
-#                 yPosition = (index / self.gridSide) * self.cellSize
-
-#                 if self.grids[self.currentGridIndex].grid[index]:
-#                     image = self.aliveImage
-#                 else:
-#                     image = self.deadImage
-
-#                 self.window.blit(image, (xPosition, yPosition))
-
-#     def setCell(self, position):
-
-#         xPosition = position[0] / self.cellSize
-#         yPosition = position[1] / self.cellSize
-
-#         index = (xPosition + (yPosition * self.gridSide))
-
-#         self.grids[self.currentGridIndex].toggleCell(index)
-
-#     def resetGrids(self):
-
-#         self.grids[0].clearGrid()
-#         self.grids[1].clearGrid()
-
-#     def printGrids(self):
-
-#         self.grids[0].printGrid()
-#         self.grids[1].printGrid()
-
 # Free functions
 
 def drawText(text, xPostion, yPosition):
@@ -206,6 +26,8 @@ def increaseClockRate(delay, step):
 
     if delay > 0:
         return delay - step
+    else:
+        return delay
 
 def reset(automaton):
 
@@ -245,6 +67,7 @@ def onButton(mousePosition, buttonPosition, buttonDimensions):
     else:
         return False
 
+
 # Setup
 
 pygame.init()
@@ -266,8 +89,6 @@ buttonPositions.append(Constants.STOP_BUTTON_LOCATION)
 buttonPositions.append(Constants.FASTER_BUTTON_LOCATION)
 buttonPositions.append(Constants.SLOWER_BUTTON_LOCATION)
 
-print(buttonPositions)
-print(buttonDimensions)
 
 for index in range(0, Constants.TOTAL_INDICES):
     cells.append(False)
@@ -290,6 +111,7 @@ drawText(Constants.SLOWER_LABEL, 422, 535)
 automaton = Automaton.Automaton(window, Constants.TOTAL_INDICES)
 automaton.setImages([liveCell, deadCell])
 
+run = False
 
 # Main loop
 
@@ -304,16 +126,18 @@ while True:
     for event in pygame.event.get():
 
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            automaton.setCell((xPosition, yPosition))
+            
+            if not run:
+                automaton.setCell((xPosition, yPosition))
 
             if overButton == Constants.START_BUTTON:
-                print("start")
+                run = True
             if overButton == Constants.STOP_BUTTON:
-                print("stop")
+                run = False
             if overButton == Constants.FASTER_BUTTON:
-                print("faster")
+                Constants.DEFAULT_DELAY = increaseClockRate(Constants.DEFAULT_DELAY, Constants.DELAY_STEP)
             if overButton == Constants.SLOWER_BUTTON:
-                print("slower")
+                Constants.DEFAULT_DELAY = decreaseClockRate(Constants.DEFAULT_DELAY, Constants.DELAY_STEP)
 
             
         if event.type == pygame.QUIT:
@@ -323,4 +147,7 @@ while True:
 
 
     # automaton.updateGrid()
-    # sleep(1)
+
+    if run:
+        sleep(Constants.DEFAULT_DELAY)
+        print("!!!")
